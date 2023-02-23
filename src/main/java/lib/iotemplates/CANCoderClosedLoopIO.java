1f83da8a609860b0c775dc4d94ce9ee77f136c8b
Grig84 <44533697+Grig84@users.noreply.github.com>
2023-02-23 16:32:31 -0600

    Fix Suggestions
    * Nothing eventful was changed

--- src/main/java/lib/iotemplates/CANCoderClosedLoopIO.java
+++ src/main/java/lib/iotemplates/CANCoderClosedLoopIO.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.CANCoder;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
-import frc.robot.Constants;
 
 public class CANCoderClosedLoopIO implements ClosedLoopIO {
 
